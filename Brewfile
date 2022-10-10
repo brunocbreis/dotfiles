a18fd06dca7c307987160dcda048f39674a30619
Bruno Reis <85531625+brunocbreis@users.noreply.github.com>
2022-10-10 11:27:22 -0300

    Installs via brewfile now

---
+++ Brewfile
@@ -0,0 +1,30 @@
+# Taps (repos)
+tap "charmbracelet/tap"
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+
+# Brews (packages)
+brew "bat"
+brew "unbound"
+brew "srt"
+brew "ffmpeg"
+brew "fzf"
+brew "git"
+brew "go"
+brew "gum"
+brew "python@3.10", link: false
+brew "httpie"
+brew "jq"
+brew "pango"
+brew "lilypond"
+brew "lua"
+brew "pv"
+brew "tree"
+brew "vim"
+brew "zsh"
+
+# Casks (apps)
+cask "4k-video-downloader"
+cask "raycast"
+cask "visual-studio-code"
--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,23 +4,4 @@ echo "\n<<< Starting Homebrew Setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# programming langs
-brew install lua
-brew install python
-brew install go
-
-# cmd line stuff
-brew install git
-brew install httpie
-brew install bat
-brew install fzf
-brew install gum
-
-# video stuff
-brew install ffmpeg
-brew install srt
-
-# casks
-brew install --no-quarantine raycast
-brew install visual-studio-code
-brew install 4k-video-downloader
+brew bundle -v
