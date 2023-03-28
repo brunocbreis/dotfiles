9c5cb30316dbf841818842ebb0ee379a00bce233
Bruno Reis <85531625+brunocbreis@users.noreply.github.com>
2023-03-28 19:12:03 -0300

    add brew to path for M1

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,4 +4,7 @@ echo "\n<<< Starting Homebrew Setup >>>\n"
 
 exists brew && echo "Skipping homebrew install...\n" || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+export PATH=/opt/homebrew/bin:$PATH
+export PATH=/opt/homebrew/sbin:$PATH
+
 brew bundle -v
